599482eddf071e3ec2376df919bb0937c6c62f36
mauricio-fontoura <mauriciofontour@gmail.com>
2024-02-15 09:06:41 -0300

    Adicionado novo icone, novo botão  e ajuste do not-found

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,22 +1,18 @@
 'use strict'
-let l = new Function ('mensage','console.log(mensage)');
+let l = new Function('mensage', 'console.log(mensage)');
 //-------------------------------------------------------------------
 
 Headers['Variables']
 
-let $inputText =  document.getElementById('input-text');
-let $notFound = document.getElementById('page-not-found'); 
+let $inputText = document.getElementById('input-text');
+let $notFound = document.getElementById('page-not-found');
 let $pageDecoder = document.getElementById('page-decoder');
 let $writeDecoder = document.getElementById('write-encryption');
 let $btnCopy = document.getElementById('button-copy');
-
-let $wordCrypt = ['enter','imes','ai','ober','ufer'];
-let $ramdon = parseInt(Math.random()*4);
 //-------------------------------------------------------------------
 
-function teste () {
-    addEventListener('input', ()=> {
-        
+function teste() {
+   
         if ($inputText.value === '') {
             $pageDecoder.classList.remove('selection');
             $notFound.classList.add('selection');
@@ -24,53 +20,36 @@ function teste () {
             $pageDecoder.classList.add('selection');
             $notFound.classList.remove('selection');
         }
-    })
 }
-teste();
 
-function WriteDecoder () {
-    addEventListener('input', ()=> {
-        $writeDecoder.innerHTML = $inputText.value;
-       
-    })
+function WriteDecoder() {
+
+        $writeDecoder.value = $inputText.value;
 }
-WriteDecoder();
-
 
 function EnableEncryption() {
-   
-    let $cryptografic = $inputText.value;
-
-    $writeDecoder.innerHTML = $cryptografic.replace(/e/g,"enter").replace(/i/g,"imes").replace(/a/g,"ai").replace(/o/g,"ober").replace(/u/g,"ufat").replace(/ /g,"");
-    
-  l($cryptografic)
-
+        if ($inputText.value === ""){
+            $inputText.attributes.item("disabled");
+        } else {
+            teste();
+            WriteDecoder();
+            let $cryptografic = $inputText.value;
+            $writeDecoder.value = $cryptografic.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat").replace(/ /g, "ç");
+        }
 }
 
 function DisableEncryption() {
 
     let $cryptografic = $inputText.value;
+    $writeDecoder.value = $cryptografic.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u").replace(/ç/g, " ");
 
-    $writeDecoder.innerHTML = $cryptografic.replace(/enter/g,"e").replace(/imes/g,"i").replace(/ai/g,"a").replace(/ober/g,"o").replace(/ufat/g,"u");
-    
 }
 
 function CopyText() {
-    $writeDecoder.select()
-    $writeDecoder.setSelectionRange(0, 99999);
     navigator.clipboard.writeText($writeDecoder.value);
-    $btnCopy.innerHTML = "Texto copiado"
-    $btnCopy.style.color="crimson";
-    setTimeout(()=> {
-    $btnCopy.innerHTML = "Copiar";
-    $btnCopy.style.color="black";
-    },2000)
+    $btnCopy.innerHTML = "Texto copiado";
+    setTimeout(() => {
+        $btnCopy.innerHTML = "Copiar";
+        $btnCopy.style.color = "black";
+    }, 2000)
 }
-
-
-// A letra "e" é convertida para "enter"
-// A letra "i" é convertida para "imes"
-// A letra "a" é convertida para "ai"
-// A letra "o" é convertida para "ober"
-// A letra "u" é convertida para "ufat"
-
